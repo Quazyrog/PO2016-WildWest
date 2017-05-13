de9fda10a34fd8d893767efa4bd50876be189ed9
Wojciech Matusiak <saphir2357@gmail.com>
2017-05-13 22:58:58 +0200

    Implementacja Akcjii i licznika na nie

---
+++ src/dzikizachod/Akcja.java
@@ -0,0 +1,7 @@
+package dzikizachod.main;
+
+/**
+ * Created by saphir on 13/05/17.
+ */
+public enum Akcja {
+}
---
+++ src/dzikizachod/LicznikAkcji.java
@@ -0,0 +1,7 @@
+package dzikizachod;
+
+/**
+ * Created by saphir on 13/05/17.
+ */
+public class LicznikAkcji {
+}
