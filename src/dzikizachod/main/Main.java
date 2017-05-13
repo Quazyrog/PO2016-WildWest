efed9e4b527263e8d4798320d06c98423731341f
Wojciech Matusiak <saphir2357@gmail.com>
2017-05-13 21:48:37 +0200

    Witaj Nowy Świecie!

---
+++ src/dzikizachod/main/Main.java
@@ -0,0 +1,10 @@
+package dzikizachod.main;
+
+/**
+ * Główna klasa odpowiedzilna za uruchomienie programu.
+ */
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Witaj (Nowy) Świecie!");
+    }
+}
