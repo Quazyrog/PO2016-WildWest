6f1e9f7fb0149d2d16ae3c706b146dcd2f2815dc
Wojciech Matusiak <wm382710>
2017-05-21 15:10:38 +0200

    Pomocnicy nie leczyli szeryfa

--- src/dzikizachod/StrategiaPomocnikaSzeryfa.java
+++ src/dzikizachod/StrategiaPomocnikaSzeryfa.java
@@ -16,6 +16,10 @@ public abstract class StrategiaPomocnikaSzeryfa extends StrategiaOgolna {
 
     @Override
     void graj() throws BladKonrtoleraWyjatek {
+        if (ja().odlegloscOd(szeryf()) == 1) {
+            while (szeryf().pz() < szeryf().maksymalnePZ() && ja().ileAkcji(Akcja.ULECZ) > 0)
+                akcjaUlecz(szeryf());
+        }
         super.graj();
         if (ja().ileAkcji(Akcja.DYNAMIT) > 0 && ja().odlegloscSkierowanOd(1, szeryf()) > 4)
             akcjaDynamit();
